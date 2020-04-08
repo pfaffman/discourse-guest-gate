81fe91e82daa7f9b8873b5ae8e7f02be6e85f8f9
Michael <michael@discoursehosting.com>
2020-04-08 06:32:03 +0000

    Update version and repository info

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,6 @@
 # name: guest-gate
 # about: Force guest users to create an account by preventing them from seeing more topics
-# version: 0.2.1
-# authors: Vinoth Kannan (vinothkannan@vinkas.com) and jgujgu
-# url: https://github.com/jgujgu/discourse-guest-gate
+# version: 0.3
+# authors: Vinoth Kannan (vinothkannan@vinkas.com) and jgujgu and michael@discoursehosting.com
+# url: https://github.com/discoursehosting/discourse-guest-gate
+#
